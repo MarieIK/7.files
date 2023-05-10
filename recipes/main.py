92c5265b3ad518b0b433a68576b58927a1af273e
Maria Kuznetsova <mikuznetsova@list.ru>
2023-05-10 23:52:59 +0300

    Added Task №2

--- recipes/main.py
+++ recipes/main.py
@@ -1,23 +1,41 @@
 from pprint import pprint
 
 
-with open('recipes.txt', encoding='utf-8') as file:
-    cook_book = {}
-    for line in file:
-        dish_name = line.strip()
-        ingredients_count = int(file.readline())
-        ingredients = []
-        for _ in range(ingredients_count):
-            ingredient, count, measure = file.readline().strip().split(' | ')
-            recipe = {
-                'ingredient_name': ingredient,
-                'quantity': count,
-                'measure': measure
-            }
-            ingredients.append(recipe)
-        file.readline()
-        cook_book[dish_name] = ingredients
+def open_file():
+    with open('recipes.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for line in file:
+            dish_name = line.strip()
+            ingredients_count = int(file.readline())
+            ingredients = []
+            for _ in range(ingredients_count):
+                ingredient, quantity, measure = file.readline().strip().split(' | ')
+                recipe = {
+                    'ingredient_name': ingredient,
+                    'quantity': quantity,
+                    'measure': measure
+                }
+                ingredients.append(recipe)
+            file.readline()
+            cook_book[dish_name] = ingredients
+    return cook_book
 
-    pprint(cook_book, sort_dicts=False)
-    
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in (cook_book := open_file()):
+            for ingredient in cook_book[dish]:
+                try:
+                    shop_list[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * int(
+                        person_count)
+                except KeyError:
+                    shop_list.update({ingredient['ingredient_name']: {'measure': ingredient['measure'],
+                                                                      'quantity': int(ingredient['quantity']) * int(
+                                                                          person_count)}})
+
+    return shop_list
+
+
+pprint(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 10), sort_dicts=False)
 
